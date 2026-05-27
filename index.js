b940dd07e96123b0dc32cb052abb8831efbb8fbd
JowelislamHabib <jowel@bintofajjal.com>
2026-05-28 00:54:57 +0600

    feat(piprapay): add initial package.json for piprapay API with dependencies

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const verifyToken = async (req, res, next) => {
 
   try {
     const { payload } = await jwtVerify(token, JWKS);
-    console.log(payload);
+    // console.log(payload);
     next();
   } catch (error) {
     return res.status(401).json({ message: "Unauthorized" });
